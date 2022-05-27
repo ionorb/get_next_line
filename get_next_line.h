b8e57ec74a797da940bdf7cd68c4c6d0f0670306
Yoel RIDGWAY-LOPEZ <yridgway@e3r1p14.42.fr>
2022-05-27 14:32:23 +0200

    added the necessary include statements and prototyped the get_next_line function in the header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,17 @@
 /*   By: yridgway <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 16:00:15 by yridgway          #+#    #+#             */
-/*   Updated: 2022/05/26 16:03:34 by yridgway         ###   ########.fr       */
+/*   Updated: 2022/05/26 17:56:33 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
 char    *get_next_line(int fd);
 
