226792efc54cdba217650e3cd7170e345f5536bf
Yoel RIDGWAY-LOPEZ <yridgway@e3r1p14.42.fr>
2022-05-30 19:05:30 +0200

    removed my libft because im not allowed to use it and added a function in utils

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,14 @@
 /*   By: yridgway <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:59:49 by yridgway          #+#    #+#             */
-/*   Updated: 2022/05/26 16:03:34 by yridgway         ###   ########.fr       */
+/*   Updated: 2022/05/30 18:34:12 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
+char    *get_next_line(int fd)
+{
+    char*   buf[BUFFER_SIZE + 1];
+
+    while (!ft_hasnull(line))
+    read(fd, buf, BUFFER_SIZE);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,21 @@
 /*   By: yridgway <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 16:00:08 by yridgway          #+#    #+#             */
-/*   Updated: 2022/05/26 16:03:34 by yridgway         ###   ########.fr       */
+/*   Updated: 2022/05/30 18:33:37 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-
+int    ft_hasnull(char* str)
+{
+    int    i;
+    
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == '\n')
+            return (1);
+        i++;
+    }
+    return (0);
+}
